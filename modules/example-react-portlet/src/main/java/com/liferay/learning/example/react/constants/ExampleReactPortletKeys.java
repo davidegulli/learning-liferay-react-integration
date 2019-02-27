00dd4c819614d2af318295089341a51a95c24a67
Davide Gulli <davide.gulli@smc.it>
2019-02-27 19:34:41 +0100

    Implementazione portlet di esempio

---
+++ modules/example-react-portlet/src/main/java/com/liferay/learning/example/react/constants/ExampleReactPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.learning.example.react.constants;
+
+/**
+ * @author davide
+ */
+public class ExampleReactPortletKeys {
+
+	public static final String ExampleReact = "examplereact";
+
+}
---
+++ modules/example-react-portlet/src/main/java/com/liferay/learning/example/react/constants/ExampleReactWebKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.learning.example.react.constants;
+
+/**
+ * @author davide
+ */
+public class ExampleReactWebKeys {
+
+	public static final String BOOTSTRAP_REQUIRE = "BOOTSTRAP_REQUIRE";
+
+}
